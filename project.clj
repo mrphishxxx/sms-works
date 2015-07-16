202a1aa826658033b9a2797d1b244f2161a687d8
Wes Morgan <cap10morgan@gmail.com>
2015-07-16 12:27:41 -0600

    [WM] update some deps
    Clojure to 1.7.0
    Langohr to 3.2.0

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/tools.logging "0.3.1"]
                  [turbovote.resource-config "0.1.4"]
                  [twilio-api "1.0.0"]
-                 [com.novemberain/langohr "3.0.1"]]
+                 [com.novemberain/langohr "3.2.0"]]
   :main ^:skip-aot sms-works.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}

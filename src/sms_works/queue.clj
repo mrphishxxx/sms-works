8e562c7b8883a30bb31dce2f37e4d1e92694e51f
Eric Normand <ericwnormand@gmail.com>
2015-11-09 15:56:42 -0600

    [EN] Fix typo.

--- src/sms_works/queue.clj
+++ src/sms_works/queue.clj
@@ -24,7 +24,7 @@
           (do (Thread/sleep (* attempt 1000))
               (recur (inc attempt)))
           (do (log/error "Connecting to RabbitMQ failed. Bailing.")
-              (throw (ex-info "Connecting to RabbitMQ failed" {:attemts attempt})))))))
+              (throw (ex-info "Connecting to RabbitMQ failed" {:attempts attempt})))))))
   (reset! channel
           (let [ch (lch/open @connection)]
             (lq/declare ch (config :rabbit-mq :queue) {:exclusive false :auto-delete false :durable true})
